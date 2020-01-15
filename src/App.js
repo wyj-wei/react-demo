16f37f08761d2b9259af6a451c8ad682e2614fd1
weiyongjian <Weiyongjian123!>
2020-01-15 11:11:25 +0800

    ..

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        hello,react
+      </div>
+    );
+  }
+}
+
+export default App;
+
